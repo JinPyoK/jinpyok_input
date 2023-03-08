d9c9568c660c5657e14fdd52a9ed0bbaa96d7603
JinPyoK <kjp00552277@gmail.com>
2023-03-08 19:37:04 +0900

    use Option type

--- src/lib.rs
+++ src/lib.rs
@@ -3,43 +3,49 @@
 use std::io::stdin;
 
 // When input multiple data, return Vec<String>.. last string probably has '\n'
-pub fn input_multiple(comment: &str) -> Vec<String> {
+pub fn input_multiple(comment: &str) -> Option<Vec<String>> {
     println!("{}", comment);
-    let mut tmp = String::new();
-    match stdin().read_line(&mut tmp) {
-        Ok(_) => {},
-        Err(e) =>println!("input_s error: {}", e),
+    let mut result: String = String::new();
+    match stdin().read_line(&mut result) {
+        Ok(_) => Some(result.split_whitespace().map(|s| s.to_string()).collect()),
+        Err(e) => {
+            println!("input_multiple read_line error: {}", e);
+            None
+        }
     }
-    let result : Vec<String> = tmp.split_whitespace().map(|s| s.to_string()).collect();
-    result
 }
 
 // When input one string
-pub fn input_string(comment: &str) -> String {
+pub fn input_string(comment: &str) -> Option<String> {
     println!("{}", comment);
-    let mut tmp = String::new();
-    match stdin().read_line(&mut tmp) {
-        Ok(_) => {},
-        Err(e) =>println!("input_s error: {}", e),
+    let mut result: String = String::new();
+    match stdin().read_line(&mut result) {
+        Ok(_) => Some(result.trim().to_string()),
+        Err(e) => {
+            println!("input_string read_line error: {}", e);
+            None
+        }
     }
-    let result : String = tmp.trim().to_string();
-    result
 }
 
 // when input one of i8 ~ i64, u8 ~ u64, isize, usize
-pub fn input_s<T: std::str::FromStr>(comment: &str) -> T {
+pub fn input_s<T: std::str::FromStr>(comment: &str) -> Option<T> {
     println!("{}", comment);
-    let mut tmp = String::new();
-    match stdin().read_line(&mut tmp) {
-        Ok(_) => {},
-        Err(e) =>println!("input_s error: {}", e),
+    let mut result: String = String::new();
+    match stdin().read_line(&mut result) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("input_s read_line error: {}", e);
+            return None;
+        }
     }
-    let result : Option<T> =  match tmp.trim().parse() {
+    match result.trim().parse() {
         Ok(n) => Some(n),
-        Err(_) => None,
-
-    };
-    result.unwrap()
+        Err(_) => {
+            println!("input_s parse error");
+            None
+        }
+    }
 }
 
 // test
@@ -49,13 +55,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let test1 : i64 = input_s::<i64>("i64: ");
-        let test2 : f32 = input_s::<f32>("f32: ");
-        let test3 : i8 = input_s::<i8>("i8: ");
-        let test4 : isize = input_s::<isize>("isize: ");
-        let test5 : String = input_string("string: ");
-        let test6 : Vec<String> = input_multiple("multiple: ");
-        let temp : Vec<String> = vec!["abc".to_string(), "def".to_string()];
+        let test1: i64 = input_s::<i64>("i64: ").unwrap();
+        let test2: f32 = input_s::<f32>("f32: ").unwrap();
+        let test3: i8 = input_s::<i8>("i8: ").unwrap();
+        let test4: isize = input_s::<isize>("isize: ").unwrap();
+        let test5: String = input_string("string: ").unwrap();
+        let test6: Vec<String> = input_multiple("multiple: ").unwrap();
+        let temp: Vec<String> = vec!["abc".to_string(), "def".to_string()];
         assert_eq!(1, test1);
         assert_eq!(2.0, test2);
         assert_eq!(3, test3);
